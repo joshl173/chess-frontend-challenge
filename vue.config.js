7ba409e0921ea6f568d39ed4aea0cf79ffdcf686
joshl173 <96530341+joshl173@users.noreply.github.com>
2023-10-18 17:11:07 -0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/chess-frontend-challenge/" : "/",
+  publicPath: process.env.NODE_env === "production" ? "/chess-frontend-challenge/" : "/"
 })
